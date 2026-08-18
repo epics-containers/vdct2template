92464ec17e20aca451e569b4573a0aeb02f5e10d
Ollie Copping <oliver.copping@diamond.ac.uk>
2026-08-18 14:59:13 +0000

    coderabbitai suggestion

--- src/vdct2template/__main__.py
+++ src/vdct2template/__main__.py
@@ -75,16 +75,15 @@ def main(
     if use_builder:
         try:
             builder = builder or Path(folder.parent.parent / "etc" / "builder.py")
-            typer.echo(f"builder.py file found: {builder}")
-
             builder_txt = builder.read_text()
+            typer.echo(f"builder.py file found: {builder}")
 
         except FileNotFoundError:
             typer.echo(
                 "ERROR: Could not find builder.py."
                 " Did you run this pointing to the folder of vdb files to convert?"
             )
-            typer.Exit()
+            typer.Exit(code=1)
 
     convert(folder, builder_txt)
 
